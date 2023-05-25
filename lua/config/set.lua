f5de17cfb8b2d14d841b43fc9155d23541d8a8bf
Stanislaw Jakubowski <sjakubowski3554@gmail.com>
2023-05-25 13:05:00 +0200

    Ctrl d,u, find remaps

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -1,2 +1,10 @@
-vim.opt.nu = true
+vim.opt.nu = true 
 vim.opt.relativenumber = true
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+
+
